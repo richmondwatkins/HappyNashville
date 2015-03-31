26705deb6f099b4f1dedb92872203e82da3c4fd2
richmondwatkins <richmond@caddis.co>
2015-03-31 17:43:11 -0500

    Initial Commit

---
+++ HappyNashville/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HappyNashville
+//
+//  Created by Richmond Watkins on 3/31/15.
+//  Copyright (c) 2015 Richmond Watkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
